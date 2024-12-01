a7f532d4bd21d6e96f0fd318ce7a25605481320f
Alex Granatowicz <alex.g@oneupsales.co.uk>
2024-12-01 16:57:04 +0000

    Solving part 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,26 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let lines: Vec<&str> = input.lines().collect();
+    let mut first_list: Vec<i32> = Vec::new();
+    let mut second_list: Vec<i32> = Vec::new();
+
+    for line in lines {
+        let (first_str, second_str) = line.split_once("   ").unwrap();
+        first_list.push(first_str.parse::<i32>().unwrap());
+        second_list.push(second_str.parse::<i32>().unwrap());
+    }
+
+    first_list.sort();
+    second_list.sort();
+
+    let mut sum: u32 = 0;
+
+    for (i, _) in first_list.iter().enumerate() {
+        sum += (first_list[i] - second_list[i]).abs() as u32;
+    }
+
+    Some(sum)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -15,7 +34,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result.unwrap(), 11u32);
     }
 
     #[test]
