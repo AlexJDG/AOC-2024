aa9d6767ea9af75b567f5b7dddb0025c0eb73386
Alex Granatowicz <alex.g@oneupsales.co.uk>
2024-12-03 19:47:22 +0000

    solve(3p1)

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,7 +1,17 @@
+use regex::Regex;
+
 advent_of_code::solution!(3);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let reginald = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    Some(
+        reginald
+            .captures_iter(input)
+            .map(|captures| captures.extract())
+            .map(|(_, [lhs, rhs])| lhs.parse::<u32>().unwrap() * rhs.parse::<u32>().unwrap())
+            .sum::<u32>(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -15,7 +25,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(161));
     }
 
     #[test]
