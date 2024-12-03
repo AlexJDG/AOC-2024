5b9cf7dea4023f5d4b6cd98a77ce3890de6f03af
Alex Granatowicz <alex.g@oneupsales.co.uk>
2024-12-03 20:57:47 +0000

    solve(3p2)

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -2,20 +2,28 @@ use regex::Regex;
 
 advent_of_code::solution!(3);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let reginald = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+fn sum_mul(input: &str) -> u32 {
+    Regex::new(r"mul\((\d+),(\d+)\)")
+        .unwrap()
+        .captures_iter(input)
+        .map(|captures| captures.extract::<2>().1)
+        .map(|[lhs, rhs]| lhs.parse::<u32>().unwrap() * rhs.parse::<u32>().unwrap())
+        .sum()
+}
 
-    Some(
-        reginald
-            .captures_iter(input)
-            .map(|captures| captures.extract())
-            .map(|(_, [lhs, rhs])| lhs.parse::<u32>().unwrap() * rhs.parse::<u32>().unwrap())
-            .sum::<u32>(),
-    )
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(sum_mul(input))
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    Some(
+        Regex::new(r"(?:^|do\(\))(.*?)(?:don't\(\)|$)")
+            .unwrap()
+            .captures_iter(&input.lines().collect::<Vec<_>>().join(""))
+            .flat_map(|captures| captures.extract::<1>().1)
+            .map(sum_mul)
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -30,7 +38,9 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(48));
     }
 }
