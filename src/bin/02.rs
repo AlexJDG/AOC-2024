9d62a96d31bfdb31266baa29fb2a1273b7813a9d
Alex Granatowicz <alex.g@oneupsales.co.uk>
2024-12-03 19:29:05 +0000

    Solving part 2 of day 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,35 +1,46 @@
 advent_of_code::solution!(2);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    Some(input.lines().fold(0, |acc, line| {
-        let num_strings: Vec<_> = line.split(" ").collect();
-        let (first, rest) = num_strings.split_first().unwrap();
-
-        let mut last = first.parse::<i32>().unwrap();
-        let mut sign = None;
-
-        for el in rest.iter() {
-            let current = el.parse::<i32>().unwrap();
-
-            let diff: i32 = last - current;
-
-            if sign.is_none() {
-                sign = Some(diff > 0);
-            }
-
-            if (sign.unwrap() != (diff > 0)) || !(1..=3).contains(&diff.unsigned_abs()) {
-                return acc;
-            }
-
-            last = current
-        }
-
-        return acc + 1;
-    }))
+fn split_report(report: &str) -> Vec<&str> {
+    report.split(" ").collect()
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+fn check_levels(levels: Vec<&str>) -> bool {
+    let (first, rest) = levels.split_first().unwrap();
+
+    let mut last = first.parse::<i32>().unwrap();
+    let mut is_ascending = None;
+
+    rest.iter().all(|el| {
+        let diff = last - el.parse::<i32>().unwrap();
+        last -= diff;
+        is_ascending = is_ascending.or(Some(diff > 0));
+
+        (is_ascending.unwrap() == (diff > 0)) && (1..=3).contains(&diff.abs())
+    })
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(
+        input
+            .lines()
+            .filter(|line| check_levels(split_report(line)))
+            .count() as u32,
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(
+        input
+            .lines()
+            .filter(|line| {
+                let levels = split_report(line);
+
+                (0..levels.len())
+                    .map(|i| [&levels[..i], &levels[i + 1..]].concat())
+                    .any(check_levels)
+            })
+            .count() as u32,
+    )
 }
 
 #[cfg(test)]
@@ -45,6 +56,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
