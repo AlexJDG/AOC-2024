2aa29795ab6561ef830f01ec4a92740614d2a4f3
Alex Granatowicz <alex.g@oneupsales.co.uk>
2024-12-06 19:59:54 +0000

    solve(5p2)

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,48 +1,96 @@
 use std::collections::{HashMap, HashSet};
+use std::iter::Map;
+use std::num::ParseIntError;
+use std::str::Lines;
 
 advent_of_code::solution!(5);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let (ordering_rules, page_lists) = input.split_once("\n\n").unwrap();
+type RulesMap<'a> = HashMap<&'a str, HashSet<&'a str>>;
+type ReportLinesIterator<'a> = Map<Lines<'a>, fn(&str) -> Vec<&str>>;
 
-    let ordering_map = ordering_rules.lines().fold(
-        HashMap::new(),
-        |mut map: HashMap<&str, HashSet<&str>>, rule| {
-            let (before, after) = rule.split_once("|").unwrap();
+fn parse(input: &str) -> Option<(RulesMap, ReportLinesIterator)> {
+    if let Some((ordering_rules, page_lists)) = input.split_once("\n\n") {
+        Some((
+            ordering_rules
+                .lines()
+                .fold(HashMap::new(), |mut map: RulesMap, rule| {
+                    let (before, after) = rule.split_once("|").unwrap();
 
-            map.entry(before).or_default().insert(after);
-            map
-        },
-    );
-
-    Some(
-        page_lists
-            .lines()
-            .filter(|line| {
-                let report_chars = line.split(",").collect::<Vec<_>>();
-                report_chars.iter().enumerate().all(|(idx, char)| {
-                    if let Some(prerequisites) = ordering_map.get(char) {
-                        return !report_chars[..idx]
-                            .iter()
-                            .any(|prev_item| prerequisites.contains(prev_item));
-                    }
-                    true
-                })
-            })
-            .map(|line| {
-                let report_chars = line.split(",").collect::<Vec<_>>();
-                report_chars
-                    .get(report_chars.len() / 2)
-                    .unwrap()
-                    .parse::<u32>()
-                    .unwrap()
-            })
-            .sum(),
-    )
+                    map.entry(before).or_default().insert(after);
+                    map
+                }),
+            page_lists
+                .lines()
+                .map(|line| line.split(",").collect::<Vec<_>>()),
+        ))
+    } else {
+        None
+    }
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+fn is_report_valid(report: &Vec<&str>, rules_map: &RulesMap) -> bool {
+    report.iter().enumerate().all(|(idx, char)| {
+        if let Some(prerequisites) = rules_map.get(char) {
+            return !report[..idx]
+                .iter()
+                .any(|prev_item| prerequisites.contains(prev_item));
+        }
+        true
+    })
+}
+
+fn get_middle_page(report: &[&str]) -> Result<u32, ParseIntError> {
+    report.get(report.len() / 2).unwrap_or(&"0").parse::<u32>()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    if let Some((rules_map, reports)) = parse(input) {
+        Some(
+            reports
+                .filter(|report| is_report_valid(report, &rules_map))
+                .map(|report| get_middle_page(&report).unwrap_or(0))
+                .sum(),
+        )
+    } else {
+        None
+    }
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    if let Some((rules_map, reports)) = parse(input) {
+        Some(
+            reports
+                .filter(|report| !is_report_valid(report, &rules_map))
+                .map(|report| {
+                    let mut filtered_rules = rules_map
+                        .iter()
+                        .filter(|(before_page, _)| report.contains(before_page))
+                        .collect::<HashMap<_, _>>();
+
+                    let res = (0..(filtered_rules.len() - 1))
+                        .map(|_| {
+                            let (&next_page, _) = &filtered_rules
+                                .iter()
+                                .find(|(_, prerequisite_pages)| {
+                                    !prerequisite_pages
+                                        .iter()
+                                        .any(|prereq| filtered_rules.contains_key(prereq))
+                                })
+                                .unwrap();
+
+                            filtered_rules.remove(next_page);
+                            *next_page
+                        })
+                        .rev()
+                        .collect::<Vec<_>>();
+                    res
+                })
+                .map(|report| get_middle_page(&report).unwrap_or(0))
+                .sum(),
+        )
+    } else {
+        None
+    }
 }
 
 #[cfg(test)]
@@ -58,6 +106,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(123));
     }
 }
