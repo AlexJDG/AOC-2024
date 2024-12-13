f98c0249d4e824f1586d85b720aadc5709cb57c3
Alex Granatowicz <alex.g@oneupsales.co.uk>
2024-12-13 12:22:42 +0000

    solve(11p2)

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(11);
 
 fn parse(input: &str) -> Vec<u64> {
@@ -7,33 +9,56 @@ fn parse(input: &str) -> Vec<u64> {
         .collect()
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let mut stones = parse(input);
-
-    for _ in 0..25 {
-        stones = stones
-            .iter()
-            .flat_map(|stone| {
-                if *stone == 0 {
-                    return vec![1u64];
-                }
-
-                let stone_str = stone.to_string();
-                if stone_str.len() % 2 == 0 {
-                    let (left, right) = stone_str.split_at(stone_str.len() / 2);
-                    return vec![left.parse::<u64>().unwrap(), right.parse::<u64>().unwrap()];
-                }
-
-                vec![stone * 2024]
-            })
-            .collect()
+fn blink(stone: u64, count: u8, cache: &mut HashMap<(u64, u8), u64>) -> u64 {
+    if count == 0 {
+        return 1;
     }
 
-    Some(stones.len() as u32)
+    if cache.contains_key(&(stone, count)) {
+        return *cache.get(&(stone, count)).unwrap();
+    }
+
+    let output;
+
+    if stone == 0 {
+        output = blink(1, count - 1, cache);
+    } else {
+        let stone_str = stone.to_string();
+        if stone_str.len() % 2 == 0 {
+            let (left, right) = stone_str.split_at(stone_str.len() / 2);
+
+            output = blink(left.parse::<u64>().unwrap(), count - 1, cache)
+                + blink(right.parse::<u64>().unwrap(), count - 1, cache);
+        } else {
+            output = blink(stone * 2024, count - 1, cache);
+        }
+    }
+
+    cache.insert((stone, count), output);
+
+    output
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<u64> {
+    let mut cache = HashMap::new();
+
+    Some(
+        parse(input)
+            .iter()
+            .map(|stone| blink(*stone, 25, &mut cache))
+            .sum(),
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut cache = HashMap::new();
+
+    Some(
+        parse(input)
+            .iter()
+            .map(|stone| blink(*stone, 75, &mut cache))
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -49,6 +74,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(65601038650482));
     }
 }
