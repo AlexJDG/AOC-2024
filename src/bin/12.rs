b95da5430f5d0b2de0325c0e4349933c2101521d
Alex Granatowicz <alex.g@oneupsales.co.uk>
2024-12-18 13:21:56 +0000

    solve(12p2)

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::collections::HashSet;
 
 advent_of_code::solution!(12);
@@ -23,36 +24,87 @@ fn get_crop<'a>((y, x): (i32, i32), garden: &'a Garden) -> Option<&'a str> {
     }
 }
 
+fn count_mismatched_offsets(
+    crop: &str,
+    (start_y, start_x): (i32, i32),
+    offsets: &[(i32, i32)],
+    garden: &Garden,
+) -> u32 {
+    offsets
+        .iter()
+        .filter(|(y_offset, x_offset)| {
+            let point = (start_y + y_offset, start_x + x_offset);
+            get_crop(point, garden).is_some_and(|neighbour_crop| neighbour_crop != crop)
+        })
+        .count() as u32
+}
+
+fn sum_points(points: impl IntoIterator<Item = (i32, i32)>) -> (i32, i32) {
+    points
+        .into_iter()
+        .fold((0, 0), |(y, x), (y_o, x_o)| (y + y_o, x + x_o))
+}
+
 fn walk_plot(
     (start_y, start_x): (i32, i32),
     garden: &Garden,
     visited: &mut HashSet<(i32, i32)>,
-    corners: &Option<&mut HashSet<(i32, i32)>>,
-) -> (u32, u32) {
+) -> (u32, u32, u32) {
     let mut area = 1u32;
     let mut perimeter = 4u32;
+    let mut corners = 0;
     let crop = garden[start_y as usize][start_x as usize];
     visited.insert((start_y, start_x));
 
-    [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    let neighbour_points = [(-1, 0), (0, 1), (1, 0), (0, -1)]
         .iter()
         .filter_map(|(y_offset, x_offset)| {
             let point = (start_y + y_offset, start_x + x_offset);
-            get_crop(point, garden).map(|neighbour_crop| (point, neighbour_crop))
+            get_crop(point, garden)
+                .map(|neighbour_crop| (point, neighbour_crop, (y_offset, x_offset)))
         })
-        .filter(|(_, neighbour_crop)| *neighbour_crop == crop)
-        .for_each(|(point, _)| {
-            if !visited.contains(&point) {
-                visited.insert(point);
+        .filter(|(_, neighbour_crop, _)| *neighbour_crop == crop);
 
-                let (n_area, n_perimeter) = walk_plot(point, garden, visited, corners);
-                area += n_area;
-                perimeter += n_perimeter;
-            }
-            perimeter -= 1;
-        });
+    for (point, _, _) in neighbour_points.clone() {
+        if !visited.contains(&point) {
+            visited.insert(point);
 
-    (area, perimeter)
+            let (n_area, n_perimeter, n_corners) = walk_plot(point, garden, visited);
+            area += n_area;
+            perimeter += n_perimeter;
+            corners += n_corners
+        }
+        perimeter -= 1;
+    }
+
+    let neighbour_offsets = neighbour_points
+        .map(|(_, _, (&y, &x))| (y, x))
+        .collect::<HashSet<_>>();
+
+    if neighbour_offsets.is_empty() {
+        corners += 4;
+    } else if neighbour_offsets.len() == 1 {
+        corners += 2;
+    } else if neighbour_offsets.len() == 2 {
+        let sum = sum_points(neighbour_offsets);
+
+        if sum != (0, 0) {
+            corners += 1 + count_mismatched_offsets(crop, (start_y, start_x), &[sum], garden);
+        }
+    } else {
+        corners += count_mismatched_offsets(
+            crop,
+            (start_y, start_x),
+            &neighbour_offsets
+                .into_iter()
+                .combinations(2)
+                .map(sum_points)
+                .collect::<Vec<_>>(),
+            garden,
+        );
+    }
+
+    (area, perimeter, corners)
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -64,9 +116,8 @@ pub fn part_one(input: &str) -> Option<u32> {
     for y in 0..garden.len() {
         for x in 0..garden[y].len() {
             if !visited.contains(&(y as i32, x as i32)) {
-                let (plot_area, plot_perimeter) =
-                    walk_plot((y as i32, x as i32), &garden, &mut visited, &None);
-
+                let (plot_area, plot_perimeter, _) =
+                    walk_plot((y as i32, x as i32), &garden, &mut visited);
                 tot += plot_area * plot_perimeter;
             }
         }
@@ -76,7 +127,22 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut visited = HashSet::<(i32, i32)>::new();
+    let mut tot = 0u32;
+
+    let garden = parse(input);
+
+    for y in 0..garden.len() {
+        for x in 0..garden[y].len() {
+            if !visited.contains(&(y as i32, x as i32)) {
+                let (plot_area, _, plot_corners) =
+                    walk_plot((y as i32, x as i32), &garden, &mut visited);
+                tot += plot_area * plot_corners;
+            }
+        }
+    }
+
+    Some(tot)
 }
 
 #[cfg(test)]
@@ -92,6 +158,12 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(1206));
+    }
+
+    #[test]
+    fn test_part_two_real() {
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(808796));
     }
 }
